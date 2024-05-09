b424da8baa9e42135f384b25f53bae6d77ec1f54
karim-bakhsh <karimbakhshamiry.123@gmail.com>
2024-05-09 22:02:16 +0500

    resolved issue with searching food item

--- src/componenets/FoodLists.jsx
+++ src/componenets/FoodLists.jsx
@@ -3,50 +3,52 @@ import { useState, useEffect } from "react";
 import FoodRecipe from "./FoodRecipe";
 
 export default function FoodLists (){
-    const [formData, setFormData] = useState([])
-     const [foodName, setFoodName] = useState([])
+  const [foodName, setFoodName] = useState("chicken");
+  const [foodList, setFoodList] = useState([]);
 
-    useEffect(() => {
-        console.log("effect rendered")
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
-            .then(res => res.json())
-            .then(data => setFoodName(data.meals))
-    }, [])
+  useEffect(() => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFoodList(data.meals);
+      });
+  }, []);
 
-    function handleChange(e){
-       setFormData(e.target.value)
-    }
+  function handleChange(e) {
+    setFoodName(e.target.value);
+  }
 
-    function handleSubmit(e){
-        e.preventDefault()
-        setFoodName(foodName)
+  console.log(foodName);
 
-        console.log(foodName)
-    }
-    return (
-        <div>
-            <form onSubmit={handleSubmit}> 
-                <button><FaSearch /></button>
-                <input 
-                    type="text"
-                    placeholder="Find a recipe" 
-                    name="recipe"
-                    onChange={handleChange}
-                />
-            </form>
-            <div className="search-result">
-                {foodName.map(meal => {
-                    <FoodRecipe 
-                        meal={meal}
-                        key={meal.idMeal}
-                    />
-                })
-                }
-                
-            </div>
-          
-        </div>
-    )
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodName}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFoodList(data.meals);
+      });
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <button>
+          <FaSearch />
+        </button>
+        <input
+          type="text"
+          placeholder="Find a recipe"
+          name="food"
+          onChange={handleChange}
+        />
+      </form>
+      <div className="search-result">
+        {foodList.map((meal) => (
+          <FoodRecipe meal={meal} key={meal.idMeal} />
+        ))}
+      </div>
+    </div>
+  );
 }
     
          
